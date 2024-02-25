8aebadb49467024a766f4ea6a65390cac4a26dc0
unknown <surajpoudel@unomaha.edu>
2024-02-25 14:13:46 -0600

    testing gitignore

--- btTempletes/btTempletes.Server/Program.cs
+++ btTempletes/btTempletes.Server/Program.cs
@@ -2,10 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -15,8 +15,7 @@ app.UseStaticFiles();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
 }
 
 app.UseHttpsRedirection();
--- btTempletes/btTempletes.Server/obj/Debug/net8.0/btTempletes.Server.AssemblyInfo.cs
+++ btTempletes/btTempletes.Server/obj/Debug/net8.0/btTempletes.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("btTempletes.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2400c49159a2ad4a7fd88f9e7640a2563133dec")]
 [assembly: System.Reflection.AssemblyProductAttribute("btTempletes.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("btTempletes.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
