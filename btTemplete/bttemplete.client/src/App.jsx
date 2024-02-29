7be8c5e8aaa168800025f7d3278517bd9c645eea
kjnebel <nebelkj.13@gmail.com>
2024-02-29 11:17:09 +0900

    Updated front-end with proposal form.

--- btTemplete/bttemplete.client/src/App.jsx
+++ btTemplete/bttemplete.client/src/App.jsx
@@ -2,43 +2,54 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-    const [forecasts, setForecasts] = useState();
+    const [inputs, setForecasts] = useState();
 
     useEffect(() => {
         populateWeatherData();
     }, []);
 
-    const contents = forecasts === undefined
+    const contents = inputs === undefined
         ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tabelLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
+        : <div className="form">
+            <form>
+                <div className="input-group">
+                    <label htmlFor="name">Proposal ID:</label>
+                    <input type="text" id="propID" />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="email">Company Name:</label>
+                    <input type="text" id="compName" />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="email">Address:</label>
+                    <input type="text" id="addr" />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="email">Amount:</label>
+                    <input type="text" id="amount" />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="email">Proposal Description:</label>
+                    <input type="text" id="propDesc" />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="email">Status:</label>
+                    <input type="text" id="status" />
+                </div>
+                <button type="submit" className="submit-btn">
+                    Submit
+                </button>
+            </form>
+        </div>;
 
     return (
         <div>
-            <h1 id="tabelLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
+            <h1 id="tabelLabel">Proposal Information</h1>
+            <p>This component demonstrates filling a proposal template with inputed details.</p>
             {contents}
         </div>
     );
-    
+
     async function populateWeatherData() {
         const response = await fetch('weatherforecast');
         const data = await response.json();
