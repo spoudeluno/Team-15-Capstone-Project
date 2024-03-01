ec8c35dcef644c7a2eb8d2f8120bad09de4008c6
kjnebel <nebelkj.13@gmail.com>
2024-03-01 09:45:24 +0900

    Finished json creation from form inputs in the frontend.

--- btTemplete/bttemplete.client/src/App.jsx
+++ btTemplete/bttemplete.client/src/App.jsx
@@ -1,60 +1,69 @@
-import { useEffect, useState } from 'react';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
-    const [inputs, setForecasts] = useState();
-
-    useEffect(() => {
-        populateWeatherData();
-    }, []);
-
-    const contents = inputs === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <div className="form">
-            <form>
-                <div className="input-group">
-                    <label htmlFor="name">Proposal ID:</label>
-                    <input type="text" id="propID" />
-                </div>
-                <div className="input-group">
-                    <label htmlFor="email">Company Name:</label>
-                    <input type="text" id="compName" />
-                </div>
-                <div className="input-group">
-                    <label htmlFor="email">Address:</label>
-                    <input type="text" id="addr" />
-                </div>
-                <div className="input-group">
-                    <label htmlFor="email">Amount:</label>
-                    <input type="text" id="amount" />
-                </div>
-                <div className="input-group">
-                    <label htmlFor="email">Proposal Description:</label>
-                    <input type="text" id="propDesc" />
-                </div>
-                <div className="input-group">
-                    <label htmlFor="email">Status:</label>
-                    <input type="text" id="status" />
-                </div>
-                <button type="submit" className="submit-btn">
-                    Submit
-                </button>
-            </form>
-        </div>;
+    const [formValues, setFormValues] = useState({
+        proposalId: '',
+        companyName: '',
+        address: '',
+        amount: '',
+        description: '',
+        status: ''
+    });
+    const handleChange = (e) => {
+        setFormValues({ ...formValues, [e.target.id]: e.target.value });
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formValues);
+        //fetch('/ConfigureProposal', {
+        //    method: 'POST',
+        //    body: JSON.stringify({
+        //        formValues
+        //    }),
+        //    headers: {
+        //        'Content-type': 'application/json; charset=UTF-8',
+        //   },
+        //})
+    };
 
     return (
         <div>
             <h1 id="tabelLabel">Proposal Information</h1>
             <p>This component demonstrates filling a proposal template with inputed details.</p>
-            {contents}
+            <div className="form">
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <label htmlFor="name">Proposal ID:</label>
+                        <input type="text" id="proposalId" value={formValues.proposalId || ""} onChange={handleChange} />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="email">Company Name:</label>
+                        <input type="text" id="companyName" value={formValues.companyName || ""} onChange={handleChange} />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="email">Address:</label>
+                        <input type="text" id="address" value={formValues.address || ""} onChange={handleChange} />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="email">Amount:</label>
+                        <input type="text" id="amount" value={formValues.amount || ""} onChange={handleChange} />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="email">Proposal Description:</label>
+                        <input type="text" id="description" value={formValues.description || ""} onChange={handleChange} />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="email">Status:</label>
+                        <input type="text" id="status" value={formValues.status || ""} onChange={handleChange} />
+                    </div>
+                    <button type="submit" className="submit-btn">
+                        Submit
+                    </button>
+                </form>
+            </div>
         </div>
     );
-
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        const data = await response.json();
-        setForecasts(data);
-    }
 }
 
 export default App;
